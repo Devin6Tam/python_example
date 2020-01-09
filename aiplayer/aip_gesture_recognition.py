f21d8c29788512f4370f1ee4ba240be3f3adee00
tanxw <tanxw@kaisagroup.com>
2020-01-09 18:29:46 +0800

    ai 播放器

---
+++ aiplayer/aip_gesture_recognition.py
@@ -0,0 +1,105 @@
+#  *_* coding:utf8 *_*
+# pip install baidu-aip
+import os
+import cv2
+from aip import AipBodyAnalysis
+from threading import Thread
+
+
+""" 你的 APPID AK SK """
+APP_ID ="18233602"
+API_KEY = "tjtBeyGYsb9GSttixzOjTANb"
+SECRET_KEY = "ugyNv4nrslBliUotYU6zjyWn2d4NKjW0"
+
+gesture_client = AipBodyAnalysis(APP_ID, API_KEY, SECRET_KEY)
+
+
+def get_gesture_from_image(image_path):
+    if isinstance(image_path, str):
+        with open(image_path, "rb") as f:
+            image = f.read()
+            # print("image: ", image,)
+            # print(type(image))
+    else:
+        image = image_path
+
+    result_data = gesture_client.gesture(image)
+    if "result" in result_data and result_data["result"]:
+        result_str = result_data["result"][0]["classname"]
+    else:
+        result_str = ""
+
+    return result_str
+
+gesture_contrast = {
+    "Ok": "1",
+    "Thumb_up": "4",
+    "Thumb_down": "5",
+    "Insult": "2",
+    "Six": "3",
+    "Five": "6",
+    "Fist": "7",
+    "Honour": "8",
+}
+
+
+def gesture_testting():
+    for path in os.listdir("gesture"):
+        path_name = "gesture/" + path
+        print("图片: ", path)
+        print("手势", get_gesture_from_image(path_name), "\n")
+
+# 0指的是摄像头的编号。如果你电脑上有两个摄像头的话，访问第2个摄像头就可以传入1。
+capture = cv2.VideoCapture(0)
+def camera():
+
+    while True:
+        ret, frame = capture.read()
+        # cv2.imshow(窗口名称, 窗口显示的图像)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+Thread(target=camera).start()
+
+
+def gesture_recognition():
+    ret, frame = capture.read()
+    # nature_color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    # img = Image.fromarray(nature_color)
+    # img.save("gesture.png")
+
+    # opencv图片对象转化为二进制文件流
+    im = cv2.imencode(".png", frame)[1].tobytes()
+
+    gesture = get_gesture_from_image(im)
+
+    return gesture_contrast[gesture] if gesture in gesture_contrast else "指令未收录"
+
+def function_prompts():
+    print(
+        """
+    手势识别功能描述：
+
+        手势                                识别结果
+        -------------------------------------------------
+        ok                                    "1"
+        竖中指                                "2"
+        6                                     "3"
+        点赞                                  "4"
+        Thumb_down                            "5"
+        5                                     "6"
+        拳头                                  "7"
+        作别                                  "8"
+
+        """
+    )
+
+function_prompts()
+
+if __name__ == '__main__':
+    # gesture_testting()
+
+    while True:
+        print(gesture_recognition())
+
---
+++ aiplayer/player.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/9 15:00
+# @Author  : tanxw
+# pip install pygame
+# 标记 aiplayer 选中右键 Mark Directory as --> Source Root
+
+from aip_gesture_recognition import gesture_recognition
+# 混频器
+from pygame import mixer
+import os
+
+
+# 音乐播放路径  当前正在播放的序号  音量
+music_paths, music_no, volume = [], 0, 0.2
+
+music_names = os.listdir("music")
+
+print(music_names)
+
+for name in music_paths:
+    print(name)
+    music_paths.append("music/"+name)
+
+music_count = len(music_paths)
+
+# 初始化播放器
+mixer.init()
+
+"""
+手势识别功能描述：
+
+    手势                                识别结果                            功能设置
+    ---------------------------------------------------------------------------------------------
+    ok                                    "1"                               播放
+    竖中指                                 "2"                               暂停
+    6                                     "3"                               继续播放
+    点赞                                  "4"                               上一曲
+    Thumb_down                            "5"                               下一曲
+    5                                     "6"                               音量增加
+    拳头                                  "7"                                音量减小
+    作别                                  "8"                                退出
+
+    """
+
+# 反复操作
+while True:
+    print("""
+            1.播放
+            2.暂停
+            3.继续播放
+            4.上一曲
+            5.下一曲
+            6.音量增加
+            7.音量减小
+            8.退出
+
+    """)
+
+    # 接受指令 合法去执行，不合法重新接受指令  gesture_recognition() 手势识别
+    while True:
+        command = gesture_recognition()
+        if command in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+            print("接受的指令是：", command)
+            break
+
+    # 解析指令  完成任务
+    if command == "1":
+        mixer.music.load(music_paths[music_no])
+        mixer.music.play()
+    elif command == "2":
+        mixer.music.pause()
+    elif command == "3":
+        mixer.music.unpause()
+    elif command == "4":
+        if music_no > 0:
+            music_no -= 1
+        else:
+            music_no = music_count - 1
+        mixer.music.load(music_paths[music_no])
+        mixer.music.play()
+    elif command == "5":
+        if music_no < music_count - 1:
+            music_no += 1
+        else:
+            music_no = 0
+        mixer.music.load(music_paths[music_no])
+        mixer.music.play()
+    elif command == "6":
+        if volume < 1:
+            volume += 0.1
+            mixer.music.set_volume(volume)
+        else:
+            print("音量已经最大")
+    elif command == "7":
+        if volume > 0:
+            volume -= 0.1
+            mixer.music.set_volume(volume)
+        else:
+            print("音量已经最小")
+    elif command == "8":
+        print("正在退出播放器")
+        os.kill(os.getpid(), 9)
+
+    print("正在播放的歌曲：", music_names[music_no])
+    print("当前音量：", volume)
+
+
