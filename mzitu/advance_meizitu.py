02537f14e33685580ea7cc1d7221ae5a37de2622
tanxw <txtxw@163.com>
2020-02-23 23:52:03 +0800

    注释修改

--- mzitu/advance_meizitu.py
+++ mzitu/advance_meizitu.py
@@ -1,4 +1,5 @@
-#  *_* coding:utf8 *_*
+# *_* coding:utf8 *_*
+# @Desc 仅作为练习，学习示例，借鉴时请遵守爬虫有关法律规定
 import random
 import requests
 import os
@@ -9,12 +10,14 @@ from multiprocessing import Queue, Process
 import time
 import socket
 
+# 设置socket 默认超时时间
 socket.setdefaulttimeout(20)
 
+# 开启队列
 q = Queue()
 
-
-class GetImage(object):
+# 爬取图片对象
+class CrawlImage(object):
     def __init__(self, url, header):
         self.current_page_url = url
         self.header = header
@@ -24,6 +27,7 @@ class GetImage(object):
         self.is_dormancy = False
         self.dormancy_time = 30
 
+    # 获取当前爬取页面数据
     def get_current_page_data(self):
         if self.current_page_url:
             print("\n请求网页 {}  数据".format(self.current_page_url))
@@ -41,6 +45,7 @@ class GetImage(object):
         else:
             return ""
 
+    # 获取图片链接地址
     def get_image_url(self, q):
         print(">>>>>>>>> 开始获取图片网址 <<<<<<<<<")
         while True:
@@ -53,11 +58,13 @@ class GetImage(object):
                 q.put(None)
                 break
 
+    # 静态方法，保存图片资源信息
     @staticmethod
     def save_resource(content, save_path):
         with open(save_path, "wb") as f:
             f.write(content)
 
+    # 执行下载图片任务
     def execute_download(self, image_url, save_dir):
         response = requests.get(image_url, headers=self.header)
         response.close()
@@ -78,6 +85,7 @@ class GetImage(object):
             print("[INFO]: 图片 {} 获取失败! 数据请求网址 {} ".format(image_name, image_url))
             self.is_dormancy = True
 
+    # 开始图片下载--开启线程执行下载
     def start_download_image(self, save_dir, q):
         print("************ 开始下载图片 *************")
         while True:
@@ -99,6 +107,7 @@ class GetImage(object):
                 Thread(target=self.execute_download, args=(image_url, save_dir)).start()
                 time.sleep(random.random() / 10)
 
+    # 开启进程--获取图片链接地址，执行图片下载任务
     def download_image(self, save_dir):
         Process(target=self.get_image_url, args=(q,)).start()
         Process(target=self.start_download_image, args=(save_dir, q)).start()
@@ -113,7 +122,7 @@ mz_header = {
 }
 
 if __name__ == '__main__':
-    mz_image = GetImage(mz_url, mz_header)
+    mz_image = CrawlImage(mz_url, mz_header)
     mz_image.download_image("images")
     # 开启线程等待，以便爬去图片继续进行
     while True:
